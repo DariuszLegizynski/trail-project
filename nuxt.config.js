f5c3364846944fabcac81740d28a8e96c5f47e78
Dariusz Legizynski <d.legizynski@.com>
2023-05-30 22:18:14 +0200

    add tailwind config

--- nuxt.config.js
+++ nuxt.config.js
@@ -15,7 +15,9 @@ export default {
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [],
+  css: [
+    '@/assets/css/main.css',
+  ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [],
@@ -44,5 +46,14 @@ export default {
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {},
+  build: {
+    postcss: {
+      postcssOptions: {
+        plugins: {
+          tailwindcss: {},
+          autoprefixer: {},
+        },
+      },
+    },
+  },
 }
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <article class="h-screen w-full flex justify-between bg-red-50">
     <section>
-      <div v-for="(product, index) in products" :key="index">
+      <div v-for="(product, index) in products" :key="index" class="rounded shadow p-2 bg-blue-300">
         <h2>{{product.title}}</h2>
         <img :src="product.thumbnail" :alt="product.category" />
         <span>{{product.description}}</span>
@@ -31,7 +31,6 @@ export default {
       result.data.products.forEach(product => {
         this.products.push(product)
       })
-      console.log(this.products)
     }
   }
 }
