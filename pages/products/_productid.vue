2e94fe5a96a85dedb1ce4220f7426378dd7e25e6
Dariusz Legizynski <d.legizynski@.com>
2023-06-07 23:10:45 +0200

    add gap in product view

--- pages/products/_productid.vue
+++ pages/products/_productid.vue
@@ -4,7 +4,7 @@
       class="p-8 sm:my-16 font-brixtonRg inline-block w-full capitalize text-4xl text-center text-sky-400">
       {{ $t('product.hello')}}:
     </h1>
-    <section v-if="product" class="py-2 px-4 sm:p-4 md:grid md:grid-cols-2">
+    <section v-if="product" class="py-2 px-4 gap-x-4 sm:p-4 md:grid md:grid-cols-2">
       <section data-cy="gallery-list" class="grid gap-x-4 grid-cols-5 sm:gap-x-0 sm:gap-y-2 sm:grid-cols-1">
         <div class="col-span-1 sm:grid sm:justify-items-center sm:grid-cols-5">
           <div v-for="(image, index) in productImages" :key="index">
